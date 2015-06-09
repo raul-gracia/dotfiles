0ab8f6eccc1240191d3d1546d100ef9cff6e5f58
Raul Gracia <raulgracialario@gmail.com>
2015-06-09 18:06:54 +0100

    Forgot to remove task...

--- install.rb
+++ install.rb
@@ -12,7 +12,7 @@ puts 'Installing dotfiles...'
 excluded_files = %w{README.md Brewfile Gemfile install.rb VimLauncher.app
 alfred-workflows setup.fish Solarized-Dark.itermcolors Solarized-Light.itermcolors}
 puts "Executing: rcup -fd . #{excluded_files.map{|f| "-x #{f}"}.join(' ')}"
-`rcup -d . #{excluded_files.map{|f| "-x #{f}"}.join(' ')}`
+`rcup -fd . #{excluded_files.map{|f| "-x #{f}"}.join(' ')}`
 
 puts 'Configuring powerline...'
 # Configure tmux-powerline
@@ -25,12 +25,10 @@ unless File.exist? File.expand_path('~/Library/Fonts/Monaco_for_Powerline.otf')
   `http https://gist.githubusercontent.com/baopham/1838072/raw/2c0e00770826e651d1e355962e751325edb0f1ee/Monaco+for+Powerline.otf > ~/Library/Fonts/Monaco_for_Powerline.otf`
 end
 
-task :configure_vim_vundle do
-  puts 'Configuring Vundle.vim...'
-  unless File.exist? File.expand_path('~/.vim/bundle/vundle')
-    `git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle`
-  end
-  `vim +BundleInstall +qall`
+puts 'Configuring Vundle.vim...'
+unless File.exist? File.expand_path('~/.vim/bundle/vundle')
+  `git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle`
 end
+`vim +BundleInstall +qall`
 
 puts 'Everything installed corectly!'
