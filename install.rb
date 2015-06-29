3fd6e413d3107b5f5261e850eca1445c0dd697a7
Raul Gracia <raulgracialario@gmail.com>
2015-06-29 13:57:41 +0100

    Use the local copy of Monaco font

--- install.rb
+++ install.rb
@@ -3,13 +3,13 @@
 puts 'Installing dotfiles...'
 excluded_files = %w{README.md Brewfile Gemfile install.rb VimLauncher.app
 alfred-workflows setup.fish Solarized-Dark.itermcolors Solarized-Light.itermcolors
-bootstrap.sh}
+bootstrap.sh Monaco_for_Powerline.otf}
 puts "Executing: rcup -fd . #{excluded_files.map{|f| "-x #{f}"}.join(' ')}"
 `rcup -fd . #{excluded_files.map{|f| "-x #{f}"}.join(' ')}`
 
 unless File.exist? File.expand_path('~/Library/Fonts/Monaco_for_Powerline.otf')
   puts 'Downloading Patched Monaco font...'
-  `http https://gist.githubusercontent.com/baopham/1838072/raw/2c0e00770826e651d1e355962e751325edb0f1ee/Monaco+for+Powerline.otf > ~/Library/Fonts/Monaco_for_Powerline.otf`
+  `cp Monaco_for_Powerline.otf > ~/Library/Fonts/Monaco_for_Powerline.otf`
 end
 
 puts 'Configuring Vundle.vim...'
