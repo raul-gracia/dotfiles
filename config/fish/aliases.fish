a72efb8a81da4e568de79743e2b0205fc5877eb0
Raul Gracia <raulgracialario@gmail.com>
2015-07-14 18:18:10 +0100

    Reordering aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,69 +1,4 @@
-# Git related
-alias g='git'
-alias gb='g branch'
-alias gpt='bundle exec rake test; and bundle exec rake jshint; and gp'
-alias gc='g c'
-alias gco='g co'
-alias gst='g st -s'
-alias gaa='g add --all'
-alias ga='g add'
-alias gup='g up'
-alias glg='g lg'
-alias gdfc='g diff --cached --color-words --patience'
-alias gdf='g diff --color-words --patience'
-alias hpr='hub pull-request -b'
-alias hlog='heroku logs -t -a'
-alias hlg='heroku logs -t'
-alias hconf='heroku config -a'
-alias hp='g push heroku (current_branch):master'
-alias current_branch="git rev-parse --abbrev-ref HEAD"
-alias magnetic_repo_name="git remote show origin -n | grep 'Fetch' | ruby -e 'puts \$stdin.read.match(/Magnetic\/(.*)\.git/)[1]'"
-alias last_commit="git rev-parse HEAD"
-alias mysql="mysql -uroot"
-alias bb="cd ~/dotfiles; and brew bundle; and cd -"
-
-function gph
-  git push $argv (current_branch):master
-end
-
-# Code review tool
-alias gp="g push origin (current_branch)"
-alias current_branch="git rev-parse --abbrev-ref HEAD"
-alias last_commit_message_long="git --no-pager log -1 --pretty=%B"
-alias last_commit_message_short="git --no-pager log -1 --pretty=%s"
-alias previous_commit_hash="git rev-parse HEAD~1"
-
-function code_review_message
-  last_commit_message_long | pipeset commit_description
-  echo "Repo: "(magnetic_repo_name)"
-Branch: "(current_branch)".
-
-"$commit_description
-end
-
-function review
-  gp
-  code_review_message | pipeset message_arg
-  codereview --rev HEAD~1 \
-    --title=(last_commit_message_short) \
-    --message=$message_arg \
-    --reviewers=CJ,stephen,rob --send_mail
-end
-
-function ammend_review
-  if test (count $argv) -eq 1
-    gp
-    code_review_message | pipeset message_arg
-    codereview --rev HEAD~1 \
-      --issue=$argv[1] \
-      --title=(last_commit_message_short) \
-      --message=$message_arg \
-      --reviewers=CJ,stephen,rob --send_mail
-  else
-    echo "Usage: $_ 4590002"
-  end
-end
-
+# Fish
 alias fr='source ~/.config/fish/config.fish > /dev/null 2>&1'
 alias fe='vim ~/.config/fish/config.fish'
 alias fexports='vim ~/.config/fish/exports.fish'
@@ -81,17 +16,103 @@ alias mag='cd ~/Documents/development/magnetic'
 alias dotfiles='cd ~/dotfiles; and vim .; and cd -'
 alias godev='cd $GOPATH/src/github.com/maliciousmind'
 
-#alias vim='vim -w ~/.vimlog "$@"'
-#alias tmux="tmux -2"
-alias http='http'
 alias docs='cd ~/Documents'
 alias upgradeall='brew update; and brew upgrade'
 
 # Rails
 alias rdb='rake db:migrate'
+alias rdbt='rake db:migrate db:test:prepare'
 alias be='bundle exec'
 alias beg='bundle exec guard -c'
 alias rs='bundle exec rails server --binding 127.0.0.1'
+
+# Git
+alias g='git'
+alias gb='g branch'
+alias gpt='bundle exec rake test; and bundle exec rake jshint; and gp'
+alias gc='g c'
+alias gco='g co'
+alias gst='g st -s'
+alias gaa='g add --all'
+alias ga='g add'
+alias gup='g up'
+alias glg='g lg'
+alias gdfc='g diff --cached --color-words --patience'
+alias gdf='g diff --color-words --patience'
+alias current_branch='git rev-parse --abbrev-ref HEAD'
+alias magnetic_repo_name="git remote show origin -n | grep 'Fetch' | ruby -e 'puts \$stdin.read.match(/Magnetic\/(.*)\.git/)[1]'"
+alias clean_local_branches='git branch --merged develop | grep -v master | grep -v develop | xargs git branch -d'
+
+alias last_commit='git rev-parse HEAD'
+function gph
+  git push $argv (current_branch):master
+end
+
+function branch_creation_commit
+  git merge-base master (current_branch)
+end
+
+# heroku
+alias hpr='hub pull-request -b'
+alias hlog='heroku logs -t -a'
+alias hlg='heroku logs -t'
+alias hconf='heroku config -a'
+alias hp='g push heroku (current_branch):master'
+
+
+alias mysql='mysql -uroot'
+alias bb='cd ~/dotfiles; and brew bundle; and cd -'
+
+
+# Code review tool
+alias gp='g push origin (current_branch)'
+alias current_branch='git rev-parse --abbrev-ref HEAD'
+alias last_commit_message_long='git --no-pager log -1 --pretty=%B'
+alias last_commit_message_short='git --no-pager log -1 --pretty=%s'
+alias previous_commit_hash='git rev-parse HEAD~1'
+
+function code_review_message
+  last_commit_message_long | pipeset commit_description
+  echo "Repo: "(magnetic_repo_name)"
+Branch: "(current_branch)".
+
+"$commit_description
+end
+
+function review
+  if test (count $argv) -eq 1
+    set revision_hash $argv[1]
+  else
+    set revision_hash (branch_creation_commit)
+  end
+  echo $revision_hash
+  gp
+  code_review_message | pipeset message_arg
+  codereview --rev $revision_hash\
+    --title=(last_commit_message_short) \
+    --message=$message_arg \
+    --reviewers=CJ,stephen,rob,dcrosta --send_mail
+end
+
+function ammend_review
+  if test (count $argv) -eq 2
+    set revision_hash $argv[2]
+  else
+    set revision_hash (branch_creation_commit)
+  end
+  if test (count $argv) -ge 1
+    gp
+    code_review_message | pipeset message_arg
+    codereview --rev $revision_hash \
+      --issue=$argv[1] \
+      --title=(last_commit_message_short) \
+      --message=$message_arg \
+      --reviewers=CJ,stephen,rob,dcrosta --send_mail
+  else
+    echo "Usage: $_ 4590002 optional_hash"
+  end
+end
+
 
 
 function pipeset --no-scope-shadowing
