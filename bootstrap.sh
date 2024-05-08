be1afd716d0257792a9299ded1195bd5bab1a2f1
Raul Gracia <raulgracialario@gmail.com>
2024-05-08 20:51:39 +0100

    Update Homebrew install path

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ fi
 
 if ! command -v brew > /dev/null 2>&1; then
   echo 'Okay, lets do this. First things first, we need to install homebrew in order to get all the stuff you need'
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo 'Homebrew was already available... skipping the installation'
   echo "Let's update it instead! (you know you haven't updated in a while)"
